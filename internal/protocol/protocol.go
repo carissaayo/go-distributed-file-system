f5eea84f460696a73e42d9ee0c7b68f698f65990
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-25 16:23:38 +0100

    writing frame

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -1,14 +1,52 @@
 package protocol
 
-import "io"
-
-const (
-	maxPayload      = 1_048_575
-	minPayload      = 2
-	fieldSizeLength = 1_048_576
+import (
+	"encoding/binary"
+	"errors"
+	"io"
 )
 
+const (
+	maxPayload      = 1_048_576
+	minPayload      = 2
+	fieldSizeLength = 4
+)
+
+var ErrInvalidPayloadLimit = errors.New("invalid payload length")
+
 func WriteFrame(w io.Writer, payload []byte) error {
+	n := len(payload)
+	if n < minPayload || n > maxPayload {
+		return ErrInvalidPayloadLimit
+	}
+
+	var hdr = make([]byte, 4)
+	binary.BigEndian.PutUint32(hdr[:], uint32(len(payload)))
+
+	p := hdr[:]
+
+	// first loop: drain the 4-byte length
+	for len(p) > 0 {
+		written, err := w.Write(p)
+		if err != nil {
+			return err
+		}
+		p = p[written:]
+	}
+
+	p = payload
+
+	// second loop: drain the payload
+	for len(p) > 0 {
+		written, err := w.Write(p)
+		if err != nil {
+			return err
+		}
+		p = p[written:]
+
+	}
+
+	return nil
 
 }
 
