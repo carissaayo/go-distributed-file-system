093281f770aad6635ff1cb8cde2e843f677d7d0f
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-25 12:29:12 +0100

    listener

--- transport/tansport.go
+++ transport/tansport.go
@@ -1,4 +1,26 @@
 package transport
 
+import (
+	"fmt"
+	"net"
+)
+
 type Transport struct {
+	remoteddr string
+	Listener  net.Listener
+}
+
+func (tp *Transport) Listen() error {
+	var err error
+
+	tp.Listener, err = net.Listen("tcp", tp.remoteddr)
+
+	if err != nil {
+		fmt.Printf("conn error: %s\n", err)
+		return err
+	}
+	fmt.Printf("conn gotten")
+
+	return nil
+
 }
