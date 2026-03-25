fd7f5721dda3cde4010d256db2f48aaaf44a6369
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-25 12:59:34 +0100

    handleConn

--- transport/tansport.go
+++ transport/tansport.go
@@ -1,7 +1,9 @@
 package transport
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net"
 )
 
@@ -16,11 +18,44 @@ func (tp *Transport) Listen() error {
 	tp.Listener, err = net.Listen("tcp", tp.listenAddr)
 
 	if err != nil {
-		fmt.Printf("conn error: %s\n", err)
 		return err
 	}
-	fmt.Printf("conn gotten")
 
 	return nil
 
+}
+
+func (tp *Transport) Accept() {
+	for {
+		conn, err := tp.Listener.Accept()
+
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+			continue
+		}
+
+		fmt.Printf("new incoming connection %+v\n", conn)
+		go tp.handleConn(conn)
+	}
+}
+
+func (tp *Transport) handleConn(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2000)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			} else {
+				fmt.Printf("TCP error: %s\n", err)
+				break
+
+			}
+		}
+
+		fmt.Printf("message: %+v\n", buf[:n])
+
+	}
 }
