7de67e615fc8fefb4e7c9da488c805e22971ef09
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-27 10:33:45 +0100

    client

---
+++ cmd/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/carissaayo/go-tcp-scratch/internal/protocol"
+)
+
+func connect() {
+	conn, err := net.Dial("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	if err = protocol.WriteFrame(conn, []byte{1, protocol.KindPING}); err != nil {
+		log.Fatal(err)
+	}
+
+	payload, err := protocol.ReadFrame(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, kind, _, err := protocol.ParsePayload(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if kind != protocol.KindPONG {
+		log.Println("invalid kind")
+		os.Exit(1)
+	}
+
+	log.Println("ok: received PONG")
+
+}
